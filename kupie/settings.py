0bc6ec150ec556672e096a5fcea5d51c309fd404
acid3 <email@com>
2022-06-30 21:00:50 +0200

    poprawiony settings - staticfiles

--- kupie/settings.py
+++ kupie/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    # BASE_DIR / 'static',
-    os.path.join(BASE_DIR, "static"),
+    BASE_DIR / 'static',
+    # os.path.join(BASE_DIR, "static"),
 ]
 
 
