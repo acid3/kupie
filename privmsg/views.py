4b258e17d895d8b94f4bbe1be13ceb262c961dc9
acid3 <acid3@microgeek.eu>
2022-08-01 20:42:10 +0200

    minor changes

--- privmsg/views.py
+++ privmsg/views.py
@@ -10,6 +10,16 @@ from product.contactForm import msgForm
 # Create your views here.
 
 
+# def RecivedPM(request):
+#     msg_rcv = prvMsg.objects.filter(
+#         reciver_id=request.user).order_by('-sendDate')
+#     template = "privmsg/odebrane.html"
+#     context = {'msgrcv': msg_rcv,
+#                }
+
+#     return render(request, template, context)
+
+
 def RecivedPM(request):
     msg_rcv = prvMsg.objects.filter(
         reciver_id=request.user).order_by('-sendDate')
@@ -59,8 +69,10 @@ def readMsg(request, msgSlug):
     template = "privmsg/wiadomosc.html"
     message = prvMsg.objects.get(slug=msgSlug)
 
-    logged_user_msg = prvMsg.objects.filter(Q(reciver_id=request.user) | (Q(sender_id=request.user)))
-    sender_msg = logged_user_msg.filter(Q(sender_id=message.sender_id) | Q(reciver_id=message.sender_id))
+    logged_user_msg = prvMsg.objects.filter(
+        Q(reciver_id=request.user) | (Q(sender_id=request.user)))
+    sender_msg = logged_user_msg.filter(
+        Q(sender_id=message.sender_id) | Q(reciver_id=message.sender_id))
     actual_msg = sender_msg.filter(Q(title=message.title))
     czat = actual_msg.order_by('sendDate')
 
